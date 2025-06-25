7536a953bc259665822415cfb6e3ece5015e4fc0
Ethan <pangyeeshen2@gmail.com>
2025-06-25 20:40:40 +0800

    init : design pattern repository

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DesignPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
