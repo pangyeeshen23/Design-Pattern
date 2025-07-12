05ee37c99629fb1b54d5d465298d5cff1c9ea146
Ethan <pangyeeshen2@gmail.com>
2025-07-12 16:08:52 +0800

    learn : copy by serialization

---
+++ Prototype/SerializationPrototype.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.Json;
+
+namespace DesignPattern.Prototype
+{
+    public static class SerializeExtension
+    {
+        public static T DeepCopy<T>(this T self)
+        {
+            string serialized = JsonSerializer.Serialize(self);
+            return JsonSerializer.Deserialize<T>(serialized);
+        }
+    }
+    public class SerializationPrototype
+    {
+
+        public class Person
+        {
+            public string[] Name { get; set; }
+            public Address Address { get; set; }
+
+            public override string ToString()
+            {
+                return $"Name: {string.Join(" ", Name)}, Address : {Address.ToString()}";
+            }
+        }
+
+        public class Address
+        {
+            public string StreetName { get; set; }
+            public int HouseNumber { get; set; }
+
+            public override string ToString()
+            {
+                return $"StreetName: {StreetName}, HouseNumber : {HouseNumber}";
+            }
+        }
+    }
+}
--- Prototypes.cs
+++ Prototypes.cs
@@ -12,27 +12,43 @@ namespace DesignPattern
             //Console.WriteLine(john);
             //Console.WriteLine(jane);
 
-            RecursivePrototype.Employee john = new RecursivePrototype.Employee(
-                new[] { "John", "Smith" }, 
-                new RecursivePrototype.Address("London Road", 123),
-                321000
-            );
-            RecursivePrototype.Employee jane = john.DeepCopy();
-            jane.Address.HouseNumber = 150;
-            Console.WriteLine(jane);
-            Console.WriteLine(john);
+            //RecursivePrototype.Employee john = new RecursivePrototype.Employee(
+            //    new[] { "John", "Smith" }, 
+            //    new RecursivePrototype.Address("London Road", 123),
+            //    321000
+            //);
+            //RecursivePrototype.Employee jane = john.DeepCopy();
+            //jane.Address.HouseNumber = 150;
+            //Console.WriteLine(jane);
+            //Console.WriteLine(john);
 
-            RecursivePrototype.Employee employee = john.DeepCopy<RecursivePrototype.Employee>();
-            Console.WriteLine(employee);
+            //RecursivePrototype.Employee employee = john.DeepCopy<RecursivePrototype.Employee>();
+            //Console.WriteLine(employee);
 
-            RecursivePrototype.Person person = john.DeepCopy<RecursivePrototype.Person>();
+            //RecursivePrototype.Person person = john.DeepCopy<RecursivePrototype.Person>();
+            //Console.WriteLine(person);
+
+
+            //RecursivePrototype.Employee johnathan = john.Copy();
+            //johnathan.Address.HouseNumber = 321;
+            //Console.WriteLine(john);
+            //Console.WriteLine(johnathan);
+
+
+            // Create Prototype By Serialization
+            SerializationPrototype.Person person = new SerializationPrototype.Person
+            {
+                Name = new[] { "John", "Smith" },
+                Address = new SerializationPrototype.Address
+                {
+                    StreetName = "London Road",
+                    HouseNumber = 123
+                }
+            };
+            SerializationPrototype.Person personCopy = person.DeepCopy();
+            personCopy.Address.HouseNumber = 321;
             Console.WriteLine(person);
-
-
-            RecursivePrototype.Employee johnathan = john.Copy();
-            johnathan.Address.HouseNumber = 321;
-            Console.WriteLine(john);
-            Console.WriteLine(johnathan);
+            Console.WriteLine(personCopy);
         }
     }
 }
