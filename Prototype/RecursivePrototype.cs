d76979c92225a92b648af56ff902cb41c8a20fb6
Ethan <pangyeeshen2@gmail.com>
2025-07-12 15:53:19 +0800

    learn : propotype pattern

--- Prototype/RecursivePrototype.cs
+++ Prototype/RecursivePrototype.cs
@@ -1,11 +1,33 @@
 ﻿
+using static DesignPattern.Prototype.RecursivePrototype;
+
 namespace DesignPattern.Prototype
 {
+
+    public static class ExtensionMethods
+    {
+        public static T DeepCopy<T>(this IDeepCopyable<T> item) where T : new()
+        {
+            return item.DeepCopy();
+        }
+    }
+
     public class RecursivePrototype
     {
+        public interface IDeepCopyable<T> where T : new()
+        {
+            void CopyTo(T target);
+
+            public T DeepCopy()
+            {
+                T copy = new T();
+                CopyTo(copy);
+                return copy;
+            }
+        }
 
 
-        public class Address
+        public class Address : IDeepCopyable<Address>
         {
             public string StreetName { get; set; }
             public int HouseNumber { get; set; }
@@ -25,12 +47,27 @@ namespace DesignPattern.Prototype
                     $"{nameof(HouseNumber)}: {HouseNumber}";
             }
 
+            public Address DeepCopy()
+            {
+                return (Address) MemberwiseClone();
+            }
+
+            public void CopyTo(Address target)
+            {
+                target.StreetName = StreetName;
+                target.HouseNumber = HouseNumber;
+            }
         }
 
-        public class Person
+        public class Person : IDeepCopyable<Person>
         {
             public string[] Names { get; set; }
             public Address Address { get; set; }
+
+            public Person()
+            {
+                
+            }
 
             public Person(string[] names, Address address)
             {
@@ -42,6 +79,53 @@ namespace DesignPattern.Prototype
             {
                 return $"{nameof(Names)}: {string.Join(",", Names)}, " +
                     $"{nameof(Address)}: {Address}";
+            }
+
+            public Person DeepCopy()
+            {
+                return new Person((string[]) Names.Clone(), Address.DeepCopy());
+            }
+
+            public void CopyTo(Person target)
+            {
+                target.Names = (string[])Names.Clone();
+                target.Address = Address.DeepCopy();
+            }
+        }
+
+        public class Employee : Person, IDeepCopyable<Employee>
+        {
+            public int Salary { get; set; }
+            public Employee()
+            {
+
+            }
+
+            public Employee(string[] names, Address address, int salary) : base(names, address)
+            {
+                Salary = salary;
+            }
+
+            public override string ToString()
+            {
+                return $"{base.ToString()}, " +
+                    $"{nameof(Salary)}: {Salary}";
+            }
+
+            public Employee Copy()
+            {
+                return new Employee(Names, Address, Salary);
+            }
+
+            public new Employee DeepCopy()
+            {
+                return new Employee((string[])Names.Clone(), Address.DeepCopy(), Salary);
+            }
+
+            public void CopyTo(Employee target)
+            {
+                base.CopyTo(target);
+                target.Salary = Salary;
             }
         }
     }
--- Prototypes.cs
+++ Prototypes.cs
@@ -6,11 +6,33 @@ namespace DesignPattern
     {
         public void Run()
         {
-            PersonPrototype.Person john = new PersonPrototype.Person(new[] {"John", "Smith"}, new PersonPrototype.Address("London Road", 123));
-            PersonPrototype.Person jane = john.DeepCopy();
-            jane.Address.HouseNumber = 321;
-            Console.WriteLine(john);
+            //PersonPrototype.Person john = new PersonPrototype.Person(new[] {"John", "Smith"}, new PersonPrototype.Address("London Road", 123));
+            //PersonPrototype.Person jane = john.DeepCopy();
+            //jane.Address.HouseNumber = 321;
+            //Console.WriteLine(john);
+            //Console.WriteLine(jane);
+
+            RecursivePrototype.Employee john = new RecursivePrototype.Employee(
+                new[] { "John", "Smith" }, 
+                new RecursivePrototype.Address("London Road", 123),
+                321000
+            );
+            RecursivePrototype.Employee jane = john.DeepCopy();
+            jane.Address.HouseNumber = 150;
             Console.WriteLine(jane);
+            Console.WriteLine(john);
+
+            RecursivePrototype.Employee employee = john.DeepCopy<RecursivePrototype.Employee>();
+            Console.WriteLine(employee);
+
+            RecursivePrototype.Person person = john.DeepCopy<RecursivePrototype.Person>();
+            Console.WriteLine(person);
+
+
+            RecursivePrototype.Employee johnathan = john.Copy();
+            johnathan.Address.HouseNumber = 321;
+            Console.WriteLine(john);
+            Console.WriteLine(johnathan);
         }
     }
 }
