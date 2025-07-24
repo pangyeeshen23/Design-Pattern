06cf4f37f7be27447ddf63414127f406ecd9821c
Ethan <pangyeeshen2@gmail.com>
2025-07-24 15:32:42 +0800

    learn : adapter pattern

---
+++ Adapter/Exercise.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Coding.Exercise
+{
+    public class Square
+    {
+        public int Side;
+    }
+
+    public interface IRectangle
+    {
+        int Width { get; }
+        int Height { get; }
+    }
+
+    public static class ExtensionMethods
+    {
+        public static int Area(this IRectangle rc)
+        {
+            return rc.Width * rc.Height;
+        }
+    }
+
+    public class SquareToRectangleAdapter : IRectangle
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public SquareToRectangleAdapter(Square square)
+        {
+            Height = square.Side;
+            Width = square.Side;
+        }
+    }
+}
