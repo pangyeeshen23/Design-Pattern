dd0a44e0ae8f3fac019493b06c95a8e98f3a3c8c
Ethan <pangyeeshen2@gmail.com>
2025-08-11 12:17:32 +0800

    learn : farcade

---
+++ Farcade/House.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Farcade
+{
+    public class House
+    {
+        private List<Wall> _walls;
+
+        public House()
+        {
+            _walls = new List<Wall>();
+        }
+
+        /// This is an example of a facade pattern because we simplied a process of a building a level
+        public void BuildALevel()
+        {
+            _walls.Add(new Wall("Red"));
+            _walls.Add(new Wall("Blue"));
+            _walls.Add(new Wall("Brown"));
+            _walls.Add(new Wall("Green"));
+
+        }
+
+        public void BuildWall(string color)
+        {
+            _walls.Add(new Wall(color));
+        }
+
+        public IEnumerable<Wall> GetWalls() => _walls;
+    }
+}
---
+++ Farcade/Wall.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Farcade
+{
+    public class Wall
+    {
+        private List<string> availableColors = new List<string> { "Red", "Blue", "Green", "Yellow" };
+
+        public string Color { get; set; }
+
+        public Wall(string color)
+        {
+            if(!availableColors.Contains(color)) throw new Exception("Color not available");
+            Color = color;
+        }
+    }
+}
