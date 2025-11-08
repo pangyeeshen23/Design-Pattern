245da9a50ffdcfcb1aeab1c940f0609af261d3c9
Pang Yee Shen <pangyeeshen2@gmail.com>
2025-11-08 10:32:52 +0800

    learn : visitor for reflective visitor

--- Visitor/IntrusiveVisitor.cs
+++ Visitor/IntrusiveVisitor.cs
@@ -1,10 +1,4 @@
-﻿using ImpromptuInterface;
-using MoreLinq.Extensions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text;
 
 namespace DesignPattern.Visitor
 {
@@ -12,61 +6,55 @@ namespace DesignPattern.Visitor
     {
         public abstract class Expression
         {
+            public abstract void Print(StringBuilder sb);
         }
 
         public class DoubleExpression : Expression
         {
-            public double Value { get; set; }
+            private double _value;
 
             public DoubleExpression(double value)
             {
-                Value = value;
+                _value = value;
             }
 
+            public override void Print(StringBuilder sb)
+            {
+                sb.Append(_value);
+            }
         }
 
         public class AdditionExpression : Expression
         {
-            public Expression Left { get; set;  }
-            public Expression Right { get; set; }
+            private Expression _left, _right;
 
             public AdditionExpression(Expression left, Expression right)
             {
                 if (left == null) throw new ArgumentNullException(paramName: nameof(left));
                 if (right == null) throw new ArgumentNullException(paramName: nameof(right));
-                this.Left = left;
-                this.Right = right;
+                this._left = left;
+                this._right = right;
             }
 
-        }
-
-        public static class ExpressionPrinter
-        {
-            public static void Print(Expression e, StringBuilder sb)
+            public override void Print(StringBuilder sb)
             {
-                if (e is DoubleExpression de)
-                {
-                    sb.Append(de.Value);
-                }
-                else if (e is AdditionExpression addItion)
-                {
-                    sb.Append("(");
-                    Print(addItion.Left, sb);
-                    sb.Append("+");
-                    Print(addItion.Right, sb);
-                    sb.Append(")");
-                }
+                sb.Append("(");
+                _left.Print(sb);
+                sb.Append('+');
+                _right.Print(sb);
+                sb.Append(")");
+
             }
         }
-
 
         public static void Run()
         {
             Expression e = new AdditionExpression(new DoubleExpression(1), new AdditionExpression(new DoubleExpression(2), new DoubleExpression(3)));
             StringBuilder sb = new StringBuilder();
-            //e.Print(sb);
-            ExpressionPrinter.Print(e, sb);
+            e.Print(sb);
             Console.WriteLine(sb);
         }
+
+
     }
 }
--- Visitor/ReflectiveVisitor.cs
+++ Visitor/ReflectiveVisitor.cs
@@ -3,59 +3,93 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static DesignPattern.Visitor.ReflectiveVisitor;
 
 namespace DesignPattern.Visitor
 {
+    using DictType = Dictionary<Type, Action<Expression, StringBuilder>>;
     public static class ReflectiveVisitor
     {
+
         public abstract class Expression
         {
-            public abstract void Print(StringBuilder sb);
         }
 
         public class DoubleExpression : Expression
         {
-            private double _value;
+            public double Value { get; set; }
 
             public DoubleExpression(double value)
             {
-                _value = value;
+                Value = value;
             }
 
-            public override void Print(StringBuilder sb)
-            {
-                sb.Append(_value);
-            }
         }
 
         public class AdditionExpression : Expression
         {
-            private Expression _left, _right;
+            public Expression Left { get; set; }
+            public Expression Right { get; set; }
 
             public AdditionExpression(Expression left, Expression right)
             {
                 if (left == null) throw new ArgumentNullException(paramName: nameof(left));
                 if (right == null) throw new ArgumentNullException(paramName: nameof(right));
-                this._left = left;
-                this._right = right;
+                this.Left = left;
+                this.Right = right;
             }
 
-            public override void Print(StringBuilder sb)
-            {
-                sb.Append("(");
-                _left.Print(sb);
-                sb.Append('+');
-                _right.Print(sb);
-                sb.Append(")");
-
-            }
         }
+
+        public static class ExpressionPrinter
+        {
+            private static DictType Actions = new DictType
+            {
+                [typeof(DoubleExpression)] = (e, sb) =>
+                {
+                    var de = (DoubleExpression)e;
+                    sb.Append(de.Value);
+                },
+                [typeof(AdditionExpression)] = (e, sb) =>
+                {
+                    var addItion = (AdditionExpression)e;
+                    sb.Append("(");
+                    Print(addItion.Left, sb);
+                    sb.Append("+");
+                    Print(addItion.Right, sb);
+                    sb.Append(")");
+                }
+            };
+
+            public static void Print(Expression e, StringBuilder sb)
+            {
+                Actions[e.GetType()].Invoke(e, sb);
+            }
+
+            //public static void Print(Expression e, StringBuilder sb)
+            //{
+            //    if (e is DoubleExpression de)
+            //    {
+            //        sb.Append(de.Value);
+            //    }
+            //    else if (e is AdditionExpression addItion)
+            //    {
+            //        sb.Append("(");
+            //        Print(addItion.Left, sb);
+            //        sb.Append("+");
+            //        Print(addItion.Right, sb);
+            //        sb.Append(")");
+            //    }
+            //}
+        }
+
 
         public static void Run()
         {
             Expression e = new AdditionExpression(new DoubleExpression(1), new AdditionExpression(new DoubleExpression(2), new DoubleExpression(3)));
             StringBuilder sb = new StringBuilder();
-            e.Print(sb);
+            //e.Print(sb);
+            ExpressionPrinter.Print(e, sb);
             Console.WriteLine(sb);
         }
     }
